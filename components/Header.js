525357d9601040896e4185a68cc1204a65e0a918
ZIGCER <96gara@gmail.com>
2021-08-05 20:43:03 +0700

    'day1'

---
+++ components/Header.js
@@ -0,0 +1,78 @@
+import Image from "next/image"
+import { SearchIcon } from "@heroicons/react/solid"
+function Header() {
+  return (
+    <header className='sticky top-0 z-50  grid grid-cols-3 bg-white shadow-md p-5 md:px-10 '>
+      <div className='relative flex items-center h-10 cursor-pointer my-auto space-x-2 text-red-400'>
+        <svg
+          className='h-10'
+          viewBox='0 0 256 276'
+          xmlns='http://www.w3.org/2000/svg'
+          preserveAspectRatio='xMidYMid'
+        >
+          <path
+            d='M238 223.1a41 41 0 01-46 35c-7-.8-13.8-3-21-7.1-10-5.5-19.8-14-31.4-26.8 18.2-22.3 29.2-42.7 33.4-61 1.9-8.5 2.2-16.2 1.3-23.4a44.7 44.7 0 00-7.4-18.7 46.5 46.5 0 00-38.9-19.6c-16 0-30.3 7.4-38.9 19.6a44.8 44.8 0 00-7.4 18.7 57.3 57.3 0 001.3 23.5c4.2 18.2 15.5 38.9 33.4 61.2A123.8 123.8 0 0185 251.3c-7.2 4.1-14.1 6.3-21 7.1a41 41 0 01-46-35c-.9-6.9-.3-13.8 2.4-21.5.9-2.8 2.2-5.5 3.6-8.8l6.4-13.8.2-.6c19-41 39.5-83 60.7-123.8l.8-1.7 6.7-12.7c2.2-4.4 4.6-8.5 7.7-12a28.8 28.8 0 0144.1 0c3 3.5 5.5 7.6 7.7 12 2.2 4.2 4.4 8.6 6.7 12.7l.8 1.7c21 41 41.4 83 60.4 124.1v.3c2.2 4.4 4.1 9.4 6.3 13.8 1.4 3.3 2.8 6 3.6 8.8 2.2 7.2 3 14 2 21.2zm-110-13c-14.9-18.7-24.6-36.3-27.9-51.2a44.5 44.5 0 01-.8-16.9c.6-4.4 2.2-8.2 4.4-11.5 5.3-7.5 14-12.2 24.3-12.2 10.2 0 19.3 4.4 24.3 12.2 2.2 3.3 3.8 7.1 4.4 11.5.8 5 .5 10.8-.8 16.9-3.4 14.6-13 32.2-27.9 51.3zm124.4-14.3l-4.2-10-6.3-14-.3-.2c-19-41.4-39.4-83.3-61-124.7l-.8-1.7c-2.2-4.1-4.4-8.5-6.6-13-2.7-4.9-5.5-10.1-9.9-15.1a44.5 44.5 0 00-35-17.1C114.5 0 102 6 93 16.6a95 95 0 00-10 15.1l-6.6 13-.8 1.6c-21.2 41.4-42 83.3-61 124.7l-.2.6-6.4 14c-1.4 3-2.7 6.4-4.1 10a58.6 58.6 0 0062 79.4 72.8 72.8 0 0027.6-9.4c11.3-6.3 22-15.4 34.2-28.7a144.9 144.9 0 0034.2 28.7 72.9 72.9 0 0034.8 10 58.5 58.5 0 0058.2-50.2 52.1 52.1 0 00-2.5-29.6z'
+            fill='currentColor'
+          ></path>
+        </svg>
+        <span className='text-2xl font-bold hidden md:inline-block'>
+          airbnb
+        </span>
+      </div>
+
+      <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm hover:shadow-lg'>
+        <input
+          type='text'
+          placeholder='Start your search'
+          className='flex-grow pl-5 bg-transparent outline-none'
+        />
+        <SearchIcon className='hidden md:inline-block h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2 ' />
+      </div>
+
+      <div className='flex justify-end items-center  gap-x-4 text-gray-500 '>
+        <p className='hidden md:inline-block cursor-pointer  rounded-full py-2 px-4 hover:bg-gray-100'>
+          Become a host
+        </p>
+        <svg
+          viewBox='0 0 16 16'
+          xmlns='http://www.w3.org/2000/svg'
+          aria-hidden='true'
+          role='presentation'
+          focusable='false'
+          className='h-4 cursor-pointer  '
+        >
+          <path d='m8.002.25a7.77 7.77 0 0 1 7.748 7.776 7.75 7.75 0 0 1 -7.521 7.72l-.246.004a7.75 7.75 0 0 1 -7.73-7.513l-.003-.245a7.75 7.75 0 0 1 7.752-7.742zm1.949 8.5h-3.903c.155 2.897 1.176 5.343 1.886 5.493l.068.007c.68-.002 1.72-2.365 1.932-5.23zm4.255 0h-2.752c-.091 1.96-.53 3.783-1.188 5.076a6.257 6.257 0 0 0 3.905-4.829zm-9.661 0h-2.75a6.257 6.257 0 0 0 3.934 5.075c-.615-1.208-1.036-2.875-1.162-4.686l-.022-.39zm1.188-6.576-.115.046a6.257 6.257 0 0 0 -3.823 5.03h2.75c.085-1.83.471-3.54 1.059-4.81zm2.262-.424c-.702.002-1.784 2.512-1.947 5.5h3.904c-.156-2.903-1.178-5.343-1.892-5.494l-.065-.007zm2.28.432.023.05c.643 1.288 1.069 3.084 1.157 5.018h2.748a6.275 6.275 0 0 0 -3.929-5.068z'></path>
+        </svg>
+        <div className='flex items-center space-x-2 border-2 p-2 rounded-full hover:shadow-lg'>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-6'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            stroke-width='2'
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            class='menu'
+          >
+            <line x1='3' y1='12' x2='21' y2='12'></line>
+            <line x1='3' y1='6' x2='21' y2='6'></line>
+            <line x1='3' y1='18' x2='21' y2='18'></line>
+          </svg>
+          <svg
+            viewBox='0 0 32 32'
+            className='h-6'
+            xmlns='http://www.w3.org/2000/svg'
+            aria-hidden='true'
+            role='presentation'
+            focusable='false'
+          >
+            <path d='m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z'></path>
+          </svg>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
